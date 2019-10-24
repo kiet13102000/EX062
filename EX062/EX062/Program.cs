29ae0dce3d6c5dfc58cc73ae546446931c8a406a
kiet <kietnguyen13102000>
2019-10-24 14:41:39 +0700

    Xong

---
+++ EX062/EX062/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX062
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack S = new Stack(5);
+            S.Push(1);	//Stack contains 1
+            S.Push(2);	//Stack contains 1 2
+            S.Push(3);	//Stack contains 1 2 3
+            Console.WriteLine(S.Pop()); //Prints 3, stack contains 1 2
+            Console.WriteLine(S.Pop()); //Prints 2, stack contains 1
+            Console.WriteLine(S.Count());
+
+        }
+    }
+}
---
+++ EX062/EX062/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX062
+{
+    class Stack
+    {
+        public int[] Buffer;
+        public int top;
+        public Stack (int size)
+        {
+            Buffer = new int[size];
+        }
+       
+        
+        public void Push(int a)
+        {
+          Buffer[top++] = a;
+        }
+        public int Pop()
+        {
+            return Buffer[--top];
+        }
+        public int Peek()
+        {
+            return Buffer[top];
+        }
+        public int Count()
+        {
+            return top;
+        }
+        public void clear()
+        {
+            top = -1;
+        }
+    }
+}
